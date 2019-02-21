ba9968f60bbd6fc2b5a5c2db2e512ce696153c82
hirsbrrebekk <rebekka.hirsbrunner@iw.htwchur.ch>
2019-02-21 07:25:53 +0100

    Function clickcount programmiert

--- 02_javascript_clientside/02_starterpackage/js/klickzaehler.js
+++ 02_javascript_clientside/02_starterpackage/js/klickzaehler.js
@@ -1,3 +1,13 @@
+
 function klickZaehler() {
-  console.log("Hallo Velo");
+  if (typeof(Storage) !== "undefined"){
+  	if (localStorage.clickcount){
+  		localStorage.clickcount = Number (localStorage.clickcount)+1;
+  	} else {
+  	localStorage.clickcount=1;
+  }
+  document.getElementById("result").innerHTML = localStorage.clickcount;
+ } else {
+	document.getElementById("result").innerHTML = "Sorry, leider unterstützt dein Brower diese Funktion nicht.";
+}
 }
